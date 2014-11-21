694e39b06f3f0e70a9d76804467f8acddfe77018
Daniel Ruprecht <daniel.ruprecht@usi.ch>
2014-11-21 11:56:02 +0100

    updated runscript generator

--- scripts/build_runscript.py
+++ scripts/build_runscript.py
@@ -31,7 +31,7 @@ def build_runscript(Nproc,jobname,type,system):
             elif type=="openmp":
                 myfile.write('OMP_NUM_THREADS='+str(Nproc)+' mpirun -n 1 '+cwd+'/bin/run_parareal_openmp.out \n')
             elif type=="openmp_pipe":
-                myfile.write('OMP_NUM_THREADS='+str(Nproc)+' mpirun -n 1 '+cwd+'/bin/parareal_openmp_pipe.out \n')
+                myfile.write('OMP_NUM_THREADS='+str(Nproc)+' mpirun -n 1 '+cwd+'/bin/run_parareal_openmp_pipe.out \n')
 
         elif system=="rosa":
             if type=="mpi":
@@ -39,7 +39,7 @@ def build_runscript(Nproc,jobname,type,system):
             elif type=="openmp":
                 myfile.write('OMP_NUM_THREADS='+str(Nproc)+' aprun -n 1 -d '+str(Nproc)+' -cc=0,2,4,6,8,10,12,14,16,18,20,22,24,26,28,30 '+cwd+'/bin/run_parareal_openmp.out \n')
             elif type=="openmp_pipe":
-                myfile.write('OMP_NUM_THREADS='+str(Nproc)+' aprun -n 1 -d '+str(Nproc)+' -cc=0,2,4,6,8,10,12,14,16,18,20,22,24,26,28,30 '+cwd+'/bin/parareal_openmp_pipe.out \n')
+                myfile.write('OMP_NUM_THREADS='+str(Nproc)+' aprun -n 1 -d '+str(Nproc)+' -cc=0,2,4,6,8,10,12,14,16,18,20,22,24,26,28,30 '+cwd+'/bin/run_parareal_openmp_pipe.out \n')
 
         elif system=="daint":
             if type=="mpi":
@@ -47,7 +47,7 @@ def build_runscript(Nproc,jobname,type,system):
             elif type=="openmp":
                 myfile.write('OMP_NUM_THREADS='+str(Nproc)+' aprun -n 1 -d '+str(Nproc)+' '+cwd+'/bin/run_parareal_openmp.out \n')
             elif type=="openmp_pipe":
-                myfile.write('OMP_NUM_THREADS='+str(Nproc)+' aprun -n 1 -d '+str(Nproc)+' '+cwd+'/bin/parareal_openmp_pipe.out \n')
+                myfile.write('OMP_NUM_THREADS='+str(Nproc)+' aprun -n 1 -d '+str(Nproc)+' '+cwd+'/bin/run_parareal_openmp_pipe.out \n')
 
         elif system=="mac":
             print "No SLURM on Mac, no runscript needed..."
