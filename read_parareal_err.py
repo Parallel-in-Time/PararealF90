875917b21b086c90fc6abfcbb4bf553c7ca6efee
Daniel Ruprecht <daniel.ruprecht@usi.ch>
2014-12-24 10:21:52 +0100

    updated read_parareal_err

--- read_parareal_err.py
+++ read_parareal_err.py
@@ -1,10 +1,13 @@
-Np = 8
+Np = 2
+Niter = 1
+
 type = 'mpi'
-nps    = '%0.2i' % Np
-nps_m1 = '%0.2i' % (Np-1)
+nps     = '%0.2i' % Np
+nps_m1  = '%0.2i' % (Np-1)
+niter_s = '%0.2i' % Niter
 
 fser = open('qend.dat')
-fpar = open('q_final_'+nps_m1+'_'+nps+'_'+type+'.dat')
+fpar = open('q_final_'+niter_s+'_'+nps_m1+'_'+nps+'_'+type+'.dat')
 
 max_err = 0.0
 while True:
