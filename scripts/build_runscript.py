5f3b80a5d4b2e95729ad78971b29035498075f75
Daniel Ruprecht <daniel.ruprecht@usi.ch>
2015-01-16 14:51:16 +0100

    build script should now no longer issue a warning for serial runs

--- scripts/build_runscript.py
+++ scripts/build_runscript.py
@@ -5,7 +5,7 @@ def build_runscript(Nproc,jobname,type,system):
         myfile.write("#!/bin/bash \n")
         myfile.write("#SBATCH --job-name="+jobname+"\n")
         myfile.write("#SBATCH --nodes=1")
-        if type=="mpi":
+        if type in ("mpi", "serial_f", "serial_g"):
             myfile.write("#SBATCH --ntasks="+str(Nproc)+"\n")
             myfile.write("#SBATCH --ntasks-per-node="+str(Nproc)+"\n")
             #myfile.write("#SBATCH --cpus-per-task=1 \n")
@@ -14,7 +14,7 @@ def build_runscript(Nproc,jobname,type,system):
             myfile.write("#SBATCH --ntasks-per-node=1 \n")
             #myfile.write("#SBATCH --cpus-per-task="+str(Nproc)+"\n")
         else:
-            print "Encountered unknown type: "+type
+            print "build_runscript: Encountered unknown type: "+type
         
         myfile.write("#SBATCH --time=00:05:00 \n")
         myfile.write("#SBATCH --mail-user=daniel.ruprecht@usi.ch \n")
@@ -65,4 +65,4 @@ def build_runscript(Nproc,jobname,type,system):
             print "No SLURM on Mac, no runscript needed..."
 
         else:
-            print "Encountered unknown string for system: "+system     
+            print "build_runscript: Encountered unknown string for system: "+system
